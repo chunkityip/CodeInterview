2030bddcd07478b43746d255bea0844e7e706330
chunkityip <alex0917lfo@hotmail.com>
2023-11-11 09:40:29 -0500

    Add LinkedListCycle class new solution : Two Pointer (slow and fast)

--- Leetcode/LinkedList/LinkedListCycle.java
+++ Leetcode/LinkedList/LinkedListCycle.java
@@ -33,4 +33,34 @@ public class LinkedListCycle {
         }
         return false;
     }
+
+    public boolean hasCycle2(ListNode head) {
+        //The cycle means there will have same node following the next pointer.
+        //The keyword is repeating node
+
+        //We can use slow and fast pointers , just think of two ppl are running
+        //when slow and fast match, and they will if it is repeating cycle , return true
+        //If fast point to null and fast next point to null , return false
+
+        //So We create two ListNode , slow = head and fast = head.next
+        //slow = head.next , fast = head.next.next
+        //if (slow == fast) return true
+
+        //Bse case
+        if (head == null || head.next == null) return false;
+
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+
 }
