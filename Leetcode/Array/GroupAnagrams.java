ef40f24510f1141cdf6d7ff2b539d8e32e795b30
chunkityip <alex0917lfo@hotmail.com>
2023-11-22 14:08:08 -0500

    Add new method in groupAnagrams class

--- Leetcode/Array/GroupAnagrams.java
+++ Leetcode/Array/GroupAnagrams.java
@@ -25,6 +25,34 @@ public class GroupAnagrams {
         return new ArrayList<>(map.values());
     }
 
+    public List<List<String>> groupAnagrams(String[] strs) {
+        //Using ASCII code to count is same number or not
 
+        if (strs.length == 0) return new ArrayList<>();
+        HashMap<String , List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            int[] count = new int[26];
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 26; i++) {
+                //sb.append('#'); is used to separate the counts of different characters.
+                //This is done to ensure that different anagram groups have distinct keys
+                sb.append(count[i]);
+                sb.append('#');
+            }
+
+            String key = sb.toString();
+
+            //if (!map.containsKey(key)) map.put(key , new ArrayList());
+            map.putIfAbsent(key , new ArrayList<>());
+
+            map.get(key).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
 
 }
