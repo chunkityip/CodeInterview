789da9df1e33e6decb3d5603260c82395ef71b0b
chunkityip <alex0917lfo@hotmail.com>
2023-12-01 11:40:08 -0500

    Add Company.JPMorgan package and MinimunSuffixFlips class

---
+++ Leetcode/Company/JPMorgan/LeastNumberOfUniqueIntegersAfterKRemovals.java
@@ -0,0 +1,34 @@
+package Leetcode.Company.JPMorgan;
+
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
+public class LeastNumberOfUniqueIntegersAfterKRemovals {
+
+    public int findLeastNumOfUniqueInts (int[] arr , int k) {
+        //This question highly asked by JPMorgan
+
+        // Using HashMap to count the duplicate number or unique integer
+        // Keys as the number, values as their duplicate number
+        // For example: 5 -> 2, 4 -> 1
+
+        HashMap<Integer , Integer> map = new HashMap<>();
+        for (int num : arr) {
+            map.put(num , map.getOrDefault(num , 0) + 1);
+        }
+
+        // Using PriorityQueue to store natural ordering which the lowest value number first
+        // For example: arr = [4,3,1,1,3,3,2], it will become: 1, 1, 2, 3
+        // k as 3 - 1 = 2
+        // 2 - 1 = 1
+        // 1 - 2 = -1, stop
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(map.values());
+        while (k > 0) {
+            k -= pq.poll();
+        }
+
+        //Return the size of the remaining integers which is the most duplicate number in the PriorityQueue
+        return k < 0 ? pq.size() + 1 : pq.size();
+    }
+}
---
+++ Leetcode/Company/JPMorgan/MinimumSuffixFlips.java
@@ -0,0 +1,32 @@
+package Leetcode.Company.JPMorgan;
+
+public class MinimumSuffixFlips {
+
+    public int minFlips(String target) {
+        //This question highly asked by JPMorgan
+
+        //Basially we are checking how many time target will become s
+        //For example target = 101 , s = 000
+
+        //First character '1' at position 1: Flip at position 1 -> ans = '1', minFlips = 1
+        //Second character '0' at position 2: Flip at position 2 -> ans = '0', minFlips = 2
+        //Third character '1' at position 3: Flip at position 3 -> ans = '1', minFlips = 3
+
+        int minFlips = 0;
+        char digit = '0';
+
+        for (char c : target.toCharArray()) {
+            while (c != digit) {
+                minFlips++;
+                digit = filps(digit);
+            }
+        }
+
+        return minFlips;
+    }
+
+    //A helper method to switch 0 or 1
+    private char filps (char c) {
+        return c == '0' ? '1' : '0';
+    }
+}
