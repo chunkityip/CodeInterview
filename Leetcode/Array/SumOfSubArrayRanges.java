c41487d037d421fd3583750583970727546fcc10
chunkityip <alex0917lfo@hotmail.com>
2023-12-06 08:41:37 -0500

    Add SumOfSubarrayRanges class

---
+++ Leetcode/Array/SumOfSubArrayRanges.java
@@ -0,0 +1,20 @@
+package Leetcode.Array;
+
+public class SumOfSubArrayRanges {
+    //This question is highly asked in JPMorgan
+
+    public long subArrayRanges(int[] nums) {
+        int len = nums.length;
+        long ans = 0;
+
+        for (int left = 0; left < len; left++) {
+            int min = left , max = left;
+            for (int right = left; right < len; right++) {
+                min = Math.min(nums[right] , min);
+                max = Math.max(nums[right] , max);
+                ans += max - min;
+            }
+        }
+        return ans;
+    }
+}
