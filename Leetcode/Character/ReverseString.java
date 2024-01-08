eef353ea79de2aec69a73eec9660e837eec082d4
chunkityip <alex0917lfo@hotmail.com>
2024-01-08 13:43:13 -0500

    Add ReverseString class

---
+++ Leetcode/Character/ReverseString.java
@@ -0,0 +1,18 @@
+package Leetcode.Character;
+
+public class ReverseString {
+
+    public void reverseString(char[] s) {
+        int first = 0;
+        int end = s.length - 1;
+
+        while (first < end) {
+            char temp = s[first];
+            s[first] = s[end];
+            s[end] = temp;
+
+            first++;
+            end--;
+        }
+    }
+}
