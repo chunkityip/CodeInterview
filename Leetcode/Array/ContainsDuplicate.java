addf2826d07701e65e48cfdac64b4101126aab83
chunkityip <alex0917lfo@hotmail.com>
2024-01-09 14:01:04 -0500

    Add my idea into ContainsDuplicate class

--- Leetcode/Array/ContainsDuplicate.java
+++ Leetcode/Array/ContainsDuplicate.java
@@ -10,6 +10,13 @@ public class ContainsDuplicate {
     }
 
     public static boolean containsDuplicate(int[] nums) {
+        /*
+          Create HashSet call set
+          Iterate each index , add it into set
+          If the current index already contain in set , return true
+
+          Iterate over , there have no match , return false
+        */
         HashSet<Integer> set = new HashSet<>();
 
         for (int i = 0; i < nums.length; i++) {
