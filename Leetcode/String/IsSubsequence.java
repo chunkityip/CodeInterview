de6a602c2e734fa51049f9e03c0a4c275c44fc06
chunkityip <alex0917lfo@hotmail.com>
2024-01-11 06:08:29 -0500

    Update IsSubsequence class

--- Leetcode/String/IsSubsequence.java
+++ Leetcode/String/IsSubsequence.java
@@ -2,38 +2,26 @@ package Leetcode.String;
 
 public class IsSubsequence {
     /*
-        We need two pointer , one point to s , one point to t
-        we also need to avoid indexOutOfBound in s and t
+        We are using two pointer , one point to s index 0 , one point to t index0
 
-        so the both pointer can't be larger than the length of s or t
+        Using while loop to iterator s length and to t length
 
-        if we found same charater from both pointer , pointer i will move forward
-        else , pointer j will move forward since we need to keep comparing
+        if we find matching in both s and t , the pointer of s move forward;
+        no matter we found the matching or not point to t will always move forward
 
-        At the end , the number of pointer i must be equals to the length of s so we know they are Subsequence
-
-
-        while the pointer i is samller than the length of s  , and
-        while the pointer j is samller than the length of t  {
-
-            if character of pointer i is equals to character of pointer j {
-            pointer i will move forward
-            }
-            pointer j will move forward
-        }
-
-        If the number of pointer i equals to the length of s , return true
+        if the number of s point equal to the length of s , it means we found all the subsequence , return true
         */
-    public boolean isSubsequence(String s , String t) {
-        int i = 0 , j = 0;
-        int sLen = s.length() , tLen = t.length();
 
-        while (i < sLen && j < tLen) {
-            if (s.charAt(i) == t.charAt(j)) {
-                i++;
+    public boolean isSubsequence(String s, String t) {
+        int sLen = s.length() , tLen = t.length();
+        int sLeft = 0 , tLeft = 0;
+
+            while (sLeft < sLen && tLeft < tLen) {
+            if (s.charAt(sLeft) == t.charAt(tLeft)) {
+                sLeft++;
             }
-            j++;
+            tLeft++;
         }
-        return i == sLen;
+            return sLeft == sLen;
     }
 }
