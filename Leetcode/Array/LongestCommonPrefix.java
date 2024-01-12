fc88e98edc1d129f82496f5316b4b3fdc2c4102c
chunkityip <alex0917lfo@hotmail.com>
2024-01-12 06:52:56 -0500

    Add LongestCommonPrefix class

---
+++ Leetcode/Array/LongestCommonPrefix.java
@@ -0,0 +1,22 @@
+package Leetcode.Array;
+
+public class LongestCommonPrefix {
+
+    public String longestCommonPrefix(String[] strs) {
+        //We're basically comparing first array to the rest of array
+        //Create a StringBuilder to store
+        //if character i of first index of strs isn't match the index of strs , we are no longer comparing it
+
+        //keep adding index into StringBuilder and finally return it as String
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < strs[0].length(); i++) {
+            for(String s: strs)
+                if(i == s.length() || s.charAt(i) != strs[0].charAt(i)) {
+                    return sb.toString();
+                }
+            sb.append(strs[0].charAt(i));
+        }
+        return sb.toString();
+    }
+}
