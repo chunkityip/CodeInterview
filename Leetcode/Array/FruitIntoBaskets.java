d7afdb9c9a5b97dcb417e7038bd7a7e31ac4e5ad
chunkityip <alex0917lfo@hotmail.com>
2024-01-15 16:33:48 -0500

    Update FruitIntoBaskets class

--- Leetcode/Array/FruitIntoBaskets.java
+++ Leetcode/Array/FruitIntoBaskets.java
@@ -24,6 +24,7 @@ public class FruitIntoBaskets {
         for (int right = 0; right < fruits.length; right++) {
             map.put(fruits[right] , map.getOrDefault(fruits[right] , 0) + 1);
 
+            //Another loop to ensure the size always within 2
             while (map.size() > 2) {
                 map.put(fruits[left] , map.get(fruits[left]) - 1);
                 if (map.get(fruits[left]) == 0) {
