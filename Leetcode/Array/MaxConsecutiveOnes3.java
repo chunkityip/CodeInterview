57ffb7d9c2476ea1c5643c993df16a895c4792bc
chunkityip <alex0917lfo@hotmail.com>
2024-01-19 05:59:29 -0500

    Add MaxConsecutiveOnes3 class

---
+++ Leetcode/Array/MaxConsecutiveOnes3.java
@@ -0,0 +1,25 @@
+package Leetcode.Array;
+
+public class MaxConsecutiveOnes3 {
+
+    /*
+    So again we set a value call countZero , if countZero is bigger than k ,
+    left pointer need to move forward and reset countZero subtract by one.
+     */
+    public int longestOnes(int[] nums, int k) {
+        int left = 0 , max = 0 , countZero = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) countZero++;
+
+            while(countZero > k) {
+                if (nums[left] == 0) {
+                    countZero--;
+                }
+                left++;
+            }
+
+            max = Math.max(max , right - left + 1);
+        }
+        return max;
+    }
+}
