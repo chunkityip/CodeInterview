711a6c051e1bd1e366b23d28aeb44e3c5b20ef7d
chunkityip <alex0917lfo@hotmail.com>
2024-01-23 08:29:50 -0500

    Update LongestRepeatingCharacterReplacement class

--- Leetcode/String/LongestRepeatingCharacterReplacement.java
+++ Leetcode/String/LongestRepeatingCharacterReplacement.java
@@ -11,7 +11,7 @@ public class LongestRepeatingCharacterReplacement {
         It means : The length of window + 1 (since index start from 0) - max (the number of same number) > k
 
         Let me explain this main:
-        If the length of sliding window subtract the number of same number , the only number left will be the number of different number
+        If the length of sliding window subtract the repeat time of duplicated number , the only number left will be the number of different number
         And if the number of different number is bigger than k , we need to move the left pointer to keep the window sliding
 
         and finally as usual : the maximum length of window will be the answer
@@ -23,9 +23,10 @@ public class LongestRepeatingCharacterReplacement {
 
         for (int right = 0; right < s.length(); right++) {
             map.put(s.charAt(right) , map.getOrDefault(s.charAt(right) , 0) + 1);
+            //map is to store the repeat time of duplicated number as value
             max = Math.max(max, map.get(s.charAt(right)));
 
-            // If the length of window substact max is bigger then k, sliding the window
+            // If the length of window subtract max is bigger than k, sliding the window
             if (right - left + 1 - max > k) {
                 map.put(s.charAt(left) , map.get(s.charAt(left)) - 1);
                 if (map.get(s.charAt(left)) == 0) {
