6662ca9f212c8a960e6f4f9c27bf08dd02d395d9
chunkityip <alex0917lfo@hotmail.com>
2024-02-05 07:53:22 -0500

    Update DistinctNumbersInEachSubarray class

--- Leetcode/Array/DistinctNumbersInEachSubarray.java
+++ Leetcode/Array/DistinctNumbersInEachSubarray.java
@@ -11,13 +11,13 @@ public class DistinctNumbersInEachSubarray {
     public int[] distinctNumbers(int[] nums, int k) {
         int[] ans = new int[nums.length - k + 1];
         Map<Integer, Integer> map = new HashMap<>();
-        int left = 0, index = 0;
+        int left = 0;
 
         for (int right = 0; right < nums.length; right++) {
             // Check if the current window size is greater than k
             if (right - left >= k) {
-                //Since HashMap didn't allow any duplicate , the size of unqiue substring length will be our answer
-                ans[index++] = map.size();
+                //Since HashMap didn't allow any duplicate , the size of unique substring length will be our answer
+                ans[left] = map.size();
                 map.put(nums[left], map.get(nums[left]) - 1);
                 if (map.get(nums[left]) == 0) {
                     map.remove(nums[left]);
@@ -29,8 +29,8 @@ public class DistinctNumbersInEachSubarray {
             map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
         }
 
-        // Handle the last subarray separately
-        ans[index] = map.size();
+        // Handle the last sub-array separately
+        ans[left] = map.size();
 
         return ans;
     }
