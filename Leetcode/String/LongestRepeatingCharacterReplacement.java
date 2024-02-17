9eb6a6b8786ff0b538eb3ff598d0f2ac846fb809
chunkityip <alex0917lfo@hotmail.com>
2024-02-17 14:21:21 -0500

    Update LongestRepeatingCharacterReplacement class

--- Leetcode/String/LongestRepeatingCharacterReplacement.java
+++ Leetcode/String/LongestRepeatingCharacterReplacement.java
@@ -23,10 +23,15 @@ public class LongestRepeatingCharacterReplacement {
 
         for (int right = 0; right < s.length(); right++) {
             map.put(s.charAt(right) , map.getOrDefault(s.charAt(right) , 0) + 1);
-            //map is to store the repeat time of duplicated number as value
-            max = Math.max(max, map.get(s.charAt(right)));
 
-            // If the length of window subtract max is bigger than k, sliding the window
+            //max is to count the max repeating character
+            max = Math.max(max , map.get(r));
+
+            //If the length of sliding window substring max repeating character ,
+            //then the left over will be the one we like to use k to replace the character
+            //For example : AABABBA. Let say the right move to 4 which is the second B : AABA(B : : Right point is here)BA
+            //right as 4 - left as 0 + 1 - max as 3 = 2 AND 3 > k which as 1
+            //So we slide left pointer by 1 and remove index left char in HashMap
             if (right - left + 1 - max > k) {
                 map.put(s.charAt(left) , map.get(s.charAt(left)) - 1);
                 if (map.get(s.charAt(left)) == 0) {
@@ -43,26 +48,22 @@ public class LongestRepeatingCharacterReplacement {
 
 // Since the English letter have 26 character , we can actually use [[Frequencies array]] to store it
 
-
     public int characterReplacement2(String s, int k) {
+        int left = 0 , max = 0 , ans = 0;
         int[] arr = new int[26];
-        int left = 0;
-        int maxCount = 0;
-        int ans = 0;
 
         for (int right = 0; right < s.length(); right++) {
-            arr[s.charAt(right) - 'A']++;
-            maxCount = Math.max(maxCount, arr[s.charAt(right) - 'A']);
+            char r = s.charAt(right);
+            arr[r - 'A']++;
+            max = Math.max(max , arr[r - 'A']);
 
-            // If the length of window substact max is bigger then k, sliding the window
-            if (right - left + 1 - maxCount > k) {
-                arr[s.charAt(left) - 'A']--;
+            if (right - left + 1 - max > k) {
+                char l = s.charAt(left);
+                arr[l - 'A']--;
                 left++;
             }
-
-            ans = Math.max(ans, right - left + 1);
+            ans = Math.max(ans , right - left + 1);
         }
-
         return ans;
     }
 }
