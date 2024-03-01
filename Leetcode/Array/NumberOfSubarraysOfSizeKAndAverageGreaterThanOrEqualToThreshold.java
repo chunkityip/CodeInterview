caa08f0163fc9db0138df2fd8432657c9797f880
chunkityip <alex0917lfo@hotmail.com>
2024-03-01 09:46:21 -0500

    Update NumberOfSubarraysOfSizeKAndAverageGreaterThanOrEqualToThreshold class

--- Leetcode/Array/NumberOfSubarraysOfSizeKAndAverageGreaterThanOrEqualToThreshold.java
+++ Leetcode/Array/NumberOfSubarraysOfSizeKAndAverageGreaterThanOrEqualToThreshold.java
@@ -2,24 +2,40 @@ package Leetcode.Array;
 
 public class NumberOfSubarraysOfSizeKAndAverageGreaterThanOrEqualToThreshold {
 
-    public int numOfSubarrays(int[] arr, int k, int threshold) {
-        int sum = 0 , ans = 0;
-        int left = 0, right = 0;
+    //Two loop version
+    public int numOfSubarrays1(int[] arr, int k, int threshold) {
+        int sum = 0 , output = 0;
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
 
-        while (right < arr.length) {
-            sum += arr[right];
-            if (right - left + 1 < k) {
-                right++;
-            } else if (right - left + 1 == k) {
-                int avg = sum / k;
-                if (avg >= threshold) {
-                    ans++;
-                }
-                sum -= arr[left];
-                left++;
-                right++;
+        if (sum / k >= threshold) output++;
+
+        for (int left = k; left < arr.length; left++) {
+            sum += arr[left] - arr[left - k];
+            if (sum / k >= threshold) output++;
+        }
+        return output;
+    }
+
+    //One loop version
+    public int numOfSubarrays2(int[] arr, int k, int threshold) {
+        int sum = 0 , output = 0;
+        // Calculate the sum of the first k elements
+        for (int i = 0; i < arr.length; i++) {
+            // Add the current element to the sum
+            sum += arr[i];
+
+            // If we have more than k elements in the sum, subtract the first element
+            if (i >= k) {
+                sum -= arr[i - k];
+            }
+
+            // If we have at least k elements in the sum, check if the average meets the threshold
+            if (i >= k - 1 && sum / k >= threshold) {
+                output++;
             }
         }
-        return ans;
+        return output;
     }
 }
