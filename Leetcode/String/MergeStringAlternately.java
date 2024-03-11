01d57013032ad44c014d147b3e42e2b909e2af95
chunkityip <alex0917lfo@hotmail.com>
2024-03-11 11:41:15 -0400

    Update MergeStringAlternately class

--- Leetcode/String/MergeStringAlternately.java
+++ Leetcode/String/MergeStringAlternately.java
@@ -1,75 +1,21 @@
 package Leetcode.String;
 
 public class MergeStringAlternately {
-
-    //My solution
     public String mergeAlternately(String word1, String word2) {
+        int pointer1 = word1.length();
+        int pointer2 = word2.length();
+        int finalLen = pointer1 > pointer2 ? pointer1 : pointer2;
         StringBuilder sb = new StringBuilder();
 
-        int i = 0;
-        int j = 0;
-
-        int oneLength = word1.length();
-        int twoLength = word2.length();
-
-        while (i < oneLength || j < twoLength) {
-            if (i < oneLength && j < twoLength) {
-                sb.append(word1.charAt(i));
-                sb.append(word2.charAt(j));
-                i++;
-                j++;
-            } else if (i == oneLength && j < twoLength) {
-                sb.append(word2.charAt(j));
-                j++;
-            } else if (i < oneLength && j == twoLength) {
-                sb.append(word1.charAt(i));
-                i++;
-            }
-        }
-
-        return sb.toString();
-    }
-
-    //Solution 1 : Two pointer (Similar as my answer , just remove the i < oneLength && j < twoLength)
-    public String mergeAlternately1(String word1, String word2) {
-        StringBuilder sb = new StringBuilder();
-        int i = 0 , j = 0;
-
-        int oneLength = word1.length();
-        int twoLength = word2.length();
-
-        while (i < oneLength || j < twoLength) {
-            if (i < oneLength) {
-                sb.append(word1.charAt(i));
-                i++;
-            }
-
-            if (j < twoLength) {
-                sb.append(word2.charAt(j));
-                j++;
-            }
-        }
-
-        return sb.toString();
-    }
-
-    //Solution 2 : One pointer by using Math.max() to check which length is longer
-    public String mergeAlternately2 (String word1, String word2) {
-        StringBuilder sb = new StringBuilder();
-
-        int oneLength = word1.length();
-        int twoLength = word2.length();
-
-        for (int i = 0; i < Math.max(oneLength , twoLength); i++) {
-            if (i < oneLength) {
+        for (int i = 0; i <= finalLen; i++) {
+            if (i < pointer1) {
                 sb.append(word1.charAt(i));
             }
 
-            if (i < twoLength) {
+            if (i < pointer2) {
                 sb.append(word2.charAt(i));
             }
         }
-
         return sb.toString();
     }
 }
