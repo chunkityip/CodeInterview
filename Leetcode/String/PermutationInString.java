d13cc4eecdc8b81400f3bd471b30ecf6b95cd5d8
chunkityip <alex0917lfo@hotmail.com>
2024-05-24 05:29:30 -0400

    Update PermutationInString

--- Leetcode/String/PermutationInString.java
+++ Leetcode/String/PermutationInString.java
@@ -5,9 +5,11 @@ import java.util.Arrays;
 public class PermutationInString {
     public boolean checkInclusion(String s1, String s2) {
         if (s1.length() > s2.length()) return false;
+
         int[] oneArray = new int[26];
         int[] twoArray = new int[26];
 
+        //store character as number
         for (int i = 0; i < s1.length(); i++) {
             oneArray[s1.charAt(i) - 'a']++;
         }
@@ -15,25 +17,23 @@ public class PermutationInString {
         for (int i = 0; i < s2.length(); i++) {
             twoArray[s2.charAt(i) - 'a']++;
 
-            /*
-            Keep subtracting index i
-            For example : s1 = "ab", s2 = "eidbaooo"
-            oneArray = 110000.....
-            twoArray = 000010001....
-
-            twoArray[s2.charAt(i - s1.length()) - 'a']--;
-            remove e , add b on it
-            twoArray = 01001000....
-
-            and so on
-             */
+            //Since we are comparing s1.length into s2
+            //s1.length meaning the length of sliding window
             if (i >= s1.length()) {
                 twoArray[s2.charAt(i - s1.length()) - 'a']--;
             }
 
-            if (Arrays.equals(oneArray , twoArray)) return true;
+            //If we found permutation , return true;
+            /*
+            How it work:
+            since we limit the length of sliding window, we insure the length is curret
+            and since at Frequencies array only store integer
+            Ex: "ab" == 11 , "ba" == 11 , "ad" = 1001 , "da" = 1001
+            so no matter the order is same or not , we don't care as long as
+            the index is same
+            */
+            if(Arrays.equals(oneArray , twoArray)) return true;
         }
         return false;
     }
-
 }
