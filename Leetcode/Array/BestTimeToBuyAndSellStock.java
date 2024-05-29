9bf4ea6904d6e7609b44fb418ae259997a0a3e01
Chunkit Yip <chunkit.yip@peopleshores.com>
2024-05-29 15:01:54 -0400

    Update BestTimeToBuyAndSellStock class

--- Leetcode/Array/BestTimeToBuyAndSellStock.java
+++ Leetcode/Array/BestTimeToBuyAndSellStock.java
@@ -6,7 +6,7 @@ public class BestTimeToBuyAndSellStock {
     One pointer to store the minPrice , another one is using loop to tearing thu length of prices
     minPrice will keep comparing , is index i of prices or minPrice
 
-    if index i of prices subtract minPrice is bigger than max , max become the index i of prices subtract minPrice
+
      */
     public int maxProfit(int[] prices) {
         int minBuy = Integer.MAX_VALUE;
