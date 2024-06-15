ca10bedd7a2b94fb70f12bc637d4c290833042b2
chunkityip <alex0917lfo@hotmail.com>
2024-06-15 06:08:01 -0400

    Update GuessNumberHigherOrLower class

--- Leetcode/Integer/GuessNumberHigherOrLower.java
+++ Leetcode/Integer/GuessNumberHigherOrLower.java
@@ -2,20 +2,23 @@ package Leetcode.Integer;
 
 public class GuessNumberHigherOrLower {
     public int guessNumber(int n) {
-        int left = 0;
+        int low = 0;
 
-        while (left <= n) {
-            int mid = left + (n - left) / 2;
-            //gues is pre-defined API int guess(int num), which returns three possible results
-            int compare = guessNumber(mid);
+        while (low <= n) {
+            int mid = low + (n - low) / 2;
+            //Using API provide by Leetcode
+            int compare = guess(mid);
+
+            // your guess is equal to the number I picked (i.e. num == pick)
             if (compare == 0) {
                 return mid;
             } else if (compare == -1) {
                 n = mid - 1;
             } else {
-                left = mid + 1;
+                low = mid + 1;
             }
         }
+        // Your guess is higher than the number I picked (i.e. num > pick)
         return -1;
     }
 
