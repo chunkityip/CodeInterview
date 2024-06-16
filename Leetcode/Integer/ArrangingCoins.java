654d1f5c774e1d5507ed63144e1f433d3058dc44
chunkityip <alex0917lfo@hotmail.com>
2024-06-16 14:45:56 -0400

    fix: fix the ArrangingConins class # changing the variable as left to low , right to high to avoid the confusion between two pointer/sliding window and binary search

--- Leetcode/Integer/ArrangingCoins.java
+++ Leetcode/Integer/ArrangingCoins.java
@@ -2,20 +2,17 @@ package Leetcode.Integer;
 
 public class ArrangingCoins {
     public int arrangeCoins(int n) {
-        //The upperbound will be n , and lower-bound will be 0 since no matter what , we always have number 0
-        long left = 0 , right = n , mid , curr;
-        while (left <= right) {
-            mid = left + (right - left) / 2;
+        long low = 0, high = n, mid, curr;
+        while (low <= high) {
+            mid = low + (high - low) / 2;
             curr = mid * (mid + 1) / 2;
-
-            if (curr == n) return (int)mid;
-
-            if (n < curr) {
-                right = mid - 1;
+            if (curr == n) return (int) mid;
+            if (curr < n) {
+                low = mid + 1;
             } else {
-                left = mid + 1;
+                high = mid - 1;
             }
         }
-        return (int)right;
+        return (int) high;
     }
 }
