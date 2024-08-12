ece3ca1480d4714489dd32e087c9de000566a627
chunkityip <alex0917lfo@hotmail.com>
2024-08-12 19:50:30 -0400

    merge

---
+++ Leetcode_Python/LinkedList/MergeTwoSortedList.py
@@ -0,0 +1,24 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def mergeTwoLists(self, list1, list2):
+    """
+    :type list1: Optional[ListNode]
+    :type list2: Optional[ListNode]
+    :rtype: Optional[ListNode]
+    """
+    dummy = node = ListNode()
+    while list1 and list2:
+        if list1.val < list2.val:
+            node.next = list1
+            list1 = list1.next
+        else:
+            node.next = list2
+            list2 = list2.next
+        node = node.next
+
+    node.next = list1 if list1 else list2
+
+    return dummy.next
