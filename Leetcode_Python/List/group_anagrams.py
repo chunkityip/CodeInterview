cbaf8d0046ede5c1ec310a65a5365b041fb74052
Chunkit Yip <chunkit.yip@peopleshores.com>
2024-08-14 16:35:30 -0400

    complete Group Anagrams in Python

---
+++ Leetcode_Python/List/group_anagrams.py
@@ -0,0 +1,14 @@
+import collections
+from typing import List
+
+
+class soulation:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = collections.defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for char in s:
+                count[ord(char) - ord('a')] += 1
+            ans[tuple(count)].append(s)
+        return ans.values()
+
