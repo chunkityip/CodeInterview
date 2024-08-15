e3b3a733fed8afd6a419277ff4fa3d4a8c9bba37
Chunkit Yip <chunkit.yip@peopleshores.com>
2024-08-15 16:05:50 -0400

    Complete concatenation_of_array in python

--- Leetcode_Java/Array/ConcatenationOfArray.java
+++ Leetcode_Java/Array/ConcatenationOfArray.java
@@ -3,19 +3,14 @@ package Leetcode_Java.Array;
 public class ConcatenationOfArray {
 
     public int[] getConcatenation(int[] nums) {
-        int len = nums.length;
-        int[] ans = new int[len * 2];
+        int length = nums.length;
+        int[] result = new int[length * 2];
 
-        for (int i = 0; i < len; i++) {
-            ans[i] = nums[i];
+        for (int i = 0; i < length; i++) {
+            result[i] = nums[i];
+            result[i + length] = nums[i];
         }
 
-        //We are adding second nums array after the first array
-        //So we need to i + len to make sure the index is right
-        for (int i = 0; i < len; i++) {
-            ans[i + len] = nums[i];
-        }
-
-        return ans;
+        return result;
     }
 }
