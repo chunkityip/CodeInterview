a934cc971eb316d913ce01810e0de95627638117
chunkityip <alex0917lfo@hotmail.com>
2024-12-02 11:42:31 -0500

    update ValidParentheses

--- Leetcode_Java/stack/ValidParentheses.java
+++ Leetcode_Java/stack/ValidParentheses.java
@@ -27,29 +27,29 @@ public class ValidParentheses {
     }
 
     // Solution two
-    class Solution {
-        public boolean isValid(String s) {
-            if (s.length() == 0) return false;
-
-            Map<Character , Character> map = new HashMap<>();
-            map.put(')' , '(');
-            map.put('}', '{');
-            map.put(']' , '[');
-
-            Stack<Character> stack = new Stack<>();
-
-            for (char c : s.toCharArray()) {
-                if (map.containsKey(c)) {
-                    if (!stack.isEmpty() && stack.peek() == map.get(c)) {
-                        stack.pop();
-                    } else {
-                        return false;
-                    }
-                } else {
-                    stack.push(c);
-                }
-            }
-            return stack.isEmpty();
-        }
-    }
+//    class Solution {
+//        public boolean isValid(String s) {
+//            if (s.length() == 0) return false;
+//
+//            Map<Character , Character> map = new HashMap<>();
+//            map.put(')' , '(');
+//            map.put('}', '{');
+//            map.put(']' , '[');
+//
+//            Stack<Character> stack = new Stack<>();
+//
+//            for (char c : s.toCharArray()) {
+//                if (map.containsKey(c)) {
+//                    if (!stack.isEmpty() && stack.peek() == map.get(c)) {
+//                        stack.pop();
+//                    } else {
+//                        return false;
+//                    }
+//                } else {
+//                    stack.push(c);
+//                }
+//            }
+//            return stack.isEmpty();
+//        }
+//    }
 }
